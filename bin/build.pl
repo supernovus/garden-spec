a58a2d4760335c72c13af77cd888762801ce066b
Timothy Totten <2009@huri.net>
2011-11-22 16:23:06 -0800

    Work in progress on the script

--- bin/build.pl
+++ bin/build.pl
@@ -2,21 +2,41 @@
 
 use Modern::Perl;
 use Garden;
+use JSON;
 
-my $garden = Garden->new(paths=>['./src']);
-open(my $list, './src/pages.list');
-my @pages = <$list>;
-close $list;
+sub slurp {
+  my $filename = shift;
+  my $text = do { local( @ARGV, $/ ) = $filename ; <> } ;
+  return $text;
+}
 
-for my $page (@pages) {
-  chomp($page);
-	say "Building $page.";
-	my $tempname = "$page/Layout";
-  my $template = $garden->get($tempname);
+sub output_html {
+  my ($page, $html) = @_;
   my $outfile = "./spec/$page.html";
-  open(my $html, '>', $outfile);
-  print $html $template->render();
-  close $html;
+  open(my $file, '>', $outfile);
+  print $file $html;
+  close $file;
+}
+
+my $pages = decode_json(slurp('./src/pages.json'));
+my $garden = Garden->new(paths=>['./src']);
+
+say "Building table of contents.";
+my $tocpage = $garden->get('index');
+my $tochtml = $tocpage->render(sections=>$pages);
+output_html('index', $tochtml);
+
+for my $section (@{$pages}) {
+  for my $page (@{$section->{pages}}) {
+    if (exists $page->{file}) {
+  	  say "Building " . $page->{file} . '.';
+    	my $tempname = $page->{file} . "/Page";
+      my $title = $page->{name};
+      my $template = $garden->get($tempname);
+      my $html = $template->render(title=>$title);
+      output_html($page->{file}, $html);
+    }
+  }
 }
 say "Done building pages.";
 
