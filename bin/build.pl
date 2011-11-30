631e923d80101f31c11f7f2c36328d4b9a571777
Timothy Totten <2009@huri.net>
2011-11-30 05:50:36 -0800

    DRAFT-1: All core syntax docs are available.

--- bin/build.pl
+++ bin/build.pl
@@ -24,10 +24,24 @@ my $garden = Garden->new(paths=>['./src']);
 
 $garden->addGlobal('VERSION', $version); 
 
-my $only; ## If this is set we only build this page.
-if (@ARGV) { $only = $ARGV[0]; }
+my $toc = 0; ## Build table of contents?
+my $sp  = 0; ## Build single page?
+my $all = 0; ## Build ALL pages?
+my %build;   ## Specific pages to build.
+if (@ARGV) { 
+  for my $arg (@ARGV) {
+    given ($arg) {
+      when (/^\-t/) { $toc = 1; }
+      when (/^\-s/) { $sp  = 1; }
+      when (/^\-a/) { $all = 1; }
+      default {
+        $build{$_} = 1;
+      }
+    }
+  }
+}
 
-if (!defined $only) {
+if ($toc || $all) {
   say "Building table of contents.";
   my $tocpage = $garden->get('index');
   my $tochtml = $tocpage->render(sections=>$pages);
@@ -37,23 +51,26 @@ if (!defined $only) {
 for my $section (@{$pages}) {
   for my $page (@{$section->{pages}}) {
     if (exists $page->{file}) {
-      if (defined $only && $only ne $page->{file}) { next; }
-  	  say "Building " . $page->{file} . '.';
-    	my $tempname = $page->{file} . "/Page";
-      my $title = $page->{name};
-      my $template = $garden->get($tempname);
-      my $html = $template->render(title=>$title);
-      output_html($page->{file}, $html);
+      if ($all || exists $build{$page->{file}}) {
+    	  say "Building " . $page->{file} . '.';
+      	my $tempname = $page->{file} . "/Page";
+        my $title = $page->{name};
+        my $template = $garden->get($tempname);
+        my $html = $template->render(title=>$title);
+        output_html($page->{file}, $html);
+      }
     }
   }
 }
 
-say "Building single page version.";
-my $single = $garden->get('single_page/index');
-$garden->addGlobal('lookup', sub { return $_[0]."/content"; } );
-pop(@{$pages});
-my $single_html = $single->render(sections=>$pages);
-output_html('single_page', $single_html);
+if ($sp || $all) {
+  say "Building single page version.";
+  my $single = $garden->get('single_page/index');
+  $garden->addGlobal('lookup', sub { return $_[0]."/content"; } );
+  pop(@{$pages});
+  my $single_html = $single->render(sections=>$pages);
+  output_html('single_page', $single_html);
+}
 
 say "Done building pages.";
 
