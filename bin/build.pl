813469daae83eb1184a2dd4ce2074c8bdf7e1144
Timothy Totten <2009@huri.net>
2011-12-06 00:43:18 -0800

    Release 1

--- bin/build.pl
+++ bin/build.pl
@@ -3,6 +3,9 @@
 use Modern::Perl;
 use Garden;
 use JSON;
+use DateTime;
+
+our $API; ## The API release number. Always the second field in VERSION.
 
 sub slurp {
   my $filename = shift;
@@ -10,20 +13,40 @@ sub slurp {
   return $text;
 }
 
-sub output_html {
-  my ($page, $html) = @_;
-  my $outfile = "./spec/$page.html";
+sub output_file {
+  my ($outfile, $content) = @_;
   open(my $file, '>', $outfile);
-  print $file $html;
+  print $file $content;
   close $file;
 }
 
+sub output_html {
+  my ($page, $content) = @_;
+  my $outfile = "./spec/$API/$page.html";
+  output_file($outfile, $content);
+}
+
 my $pages = decode_json(slurp('./src/pages.json'));
-my $version = slurp('./VERSION');
+my $version = slurp('./VERSION'); ## The release name.
 my $garden = Garden->new(paths=>['./src']);
+my @verspec = split(/\s+/, $version);
+$API = "v".$verspec[1]; ## v1, v2, etc.
+my $ver = 0;            ## The specific version (DRAFT1, RC3, REV2, etc.)
+if (@verspec > 2) {
+  $ver = $verspec[2];
+}
+my $releases;
+my $relfile = './spec/releases.json';
+if (-f $relfile) {
+  $releases = decode_json(slurp($relfile));
+}
+else {
+  $releases = {};
+}
 
 $garden->addGlobal('VERSION', $version); 
 
+my $inf = 0; ## Build the info.json file?
 my $toc = 0; ## Build table of contents?
 my $sp  = 0; ## Build single page?
 my $all = 0; ## Build ALL pages?
@@ -31,6 +54,7 @@ my %build;   ## Specific pages to build.
 if (@ARGV) { 
   for my $arg (@ARGV) {
     given ($arg) {
+      when (/^\-i/) { $inf = 1; }
       when (/^\-t/) { $toc = 1; }
       when (/^\-s/) { $sp  = 1; }
       when (/^\-a/) { $all = 1; }
@@ -39,6 +63,18 @@ if (@ARGV) {
       }
     }
   }
+}
+
+if ($inf || $all) {
+  say "Building version information file.";
+  my $date = DateTime->now();
+  my $info = {
+    "date"    => "$date",
+    "rev"     => $ver,
+  };
+  $releases->{$API} = $info;
+  my $json = JSON->new->utf8->pretty->encode($releases);
+  output_file($relfile, $json);
 }
 
 if ($toc || $all) {
